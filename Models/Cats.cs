80d8c47b89a971ab28714afe202b17efc2e7d147
bumbiimbiimbum21 <44691722+bumbiimbiimbum21@users.noreply.github.com>
2018-11-02 12:17:01 +0700

    ิbum

---
+++ Models/Cats.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace WebApplication4.Models
+{
+    public partial class Cats
+    {
+        public string CatId { get; set; }
+        public string CatName { get; set; }
+    }
+}
